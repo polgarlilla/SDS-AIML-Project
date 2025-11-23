7df57890fff5248ad61d6e149091b94aaa271ec0
tajtiagi <tajtiagnesgreta@gmail.com>
2025-11-23 13:19:33 +0100

    small fix in importing modules

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from data import read_and_inspect_data, preprocess_data, cols_to_keep, qualification_map, categoricals
+from data import (
+    read_and_inspect_data,
+    preprocess_data,
+    create_holdout_set,
+    cols_to_keep,
+    qualification_map,
+    categoricals,
+)
 
 df = read_and_inspect_data("data/Dropout_Data.xlsx", "Original Data")
 df,df_holdout=create_holdout_set(df)
